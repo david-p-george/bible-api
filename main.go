5bc16660f73a50ef686f7740d3dc7649cacb7458
David George <90811730+david-p-george@users.noreply.github.com>
2022-03-03 07:44:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"io/ioutil"
 
@@ -36,6 +37,11 @@ type WelcomeMessage struct {
 
 func main() {
 	app := fiber.New()
+	
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		welcomeMsg := WelcomeMessage{Text: "Hi, and welcome to the Bible API!",
